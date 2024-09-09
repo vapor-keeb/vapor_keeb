2e0f1e954111d5746d08dced73c2b014440b06c8
Codetector <codetector@codetector.org>
2024-09-08 22:45:25 -0700

    attemted to enable USB clk unclear if it actually worked.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 #![feature(type_alias_impl_trait)]
 #![feature(impl_trait_in_assoc_type)]
 
+use ch32_hal::{pac::{OTG_FS, RCC}, Config};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use hal::gpio::{AnyPin, Level, Output, Pin};
+use qingke::interrupt::Priority;
 use {ch32_hal as hal, panic_halt as _};
 
 #[embassy_executor::task(pool_size = 3)]
@@ -22,12 +24,34 @@ async fn blink(pin: AnyPin, interval_ms: u64) {
 
 #[embassy_executor::main(entry = "qingke_rt::entry")]
 async fn main(spawner: Spawner) -> ! {
-    let p = hal::init(Default::default());
+    // setup clocks
+    let cfg = Config {
+        rcc: ch32_hal::rcc::Config::SYSCLK_FREQ_144MHZ_HSE,
+        dma_interrupt_priority: Priority::P0,
+    };
+    let p = hal::init(cfg);
     hal::embassy::init();
 
+    ::critical_section::with(|_| {
+        RCC.ahbpcenr().modify(|w| w.set_otg_en(true));
+        // Enable USBD
+
+        OTG_FS.ctrl().modify(|w| {
+            w.0 = 0;
+            w.set_host_mode(true);
+            w.set_low_speed(false);
+            w.set_dev_pu_en(true);
+            w.set_sys_ctrl(0b11);
+        });
+
+        OTG_FS.int_en().write(|w| w.0 = 0);
+    });
+
+
+
     // GPIO
-    spawner.spawn(blink(p.PC9.degrade(), 400)).unwrap();
-    // spawner.spawn(blink(p.PB8.degrade(), 100)).unwrap();
+    spawner.spawn(blink(p.PA15.degrade(), 1000)).unwrap();
+    spawner.spawn(blink(p.PB4.degrade(), 500)).unwrap();
     loop {
         Timer::after_millis(2000).await;
     }
