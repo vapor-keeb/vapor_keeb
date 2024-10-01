8addb07e57d2b0b013a3cf69abc01ef0f8cf8936
Codetector <codetector@codetector.org>
2024-09-30 18:33:33 -0700

    Produce OBJDUMP in `run.sh`

--- run.sh
+++ run.sh
@@ -6,5 +6,9 @@ USB=${1:-/dev/ttyUSB0}
 export DEFMT_LOG=${DEFMT_LOG:-trace}
 LOG_FORMAT=${LOG_FORMAT:-'[{L}]{f:>10}:{l:<4}: {s}'}
 
+EXE=vapor_keeb
+OUT_DIR=target/riscv32imafc-unknown-none-elf/release
+
 cargo flash --release --chip CH32V307
-socat $USB,rawer,b115200 STDOUT | defmt-print --verbose -e target/riscv32imafc-unknown-none-elf/release/vapor_keeb --log-format "$LOG_FORMAT"
+riscv32-elf-objdump -dC $OUT_DIR/$EXE > $OUT_DIR/$EXE.objdump || echo 'riscv32-elf-objdump not found, skipping OBJDUMP'
+socat $USB,rawer,b115200 STDOUT | defmt-print --verbose -e $OUT_DIR/$EXE --log-format "$LOG_FORMAT"
