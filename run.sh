ee6722b784d5efc33109e1708bc9d470bac3ff38
Codetector <codetector@codetector.org>
2024-10-01 20:02:19 -0700

    gdb / log

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ OUT_DIR=target/riscv32imac-unknown-none-elf/release
 
 cargo flash --release --chip CH32V307
 riscv32-elf-objdump -dC $OUT_DIR/$EXE > $OUT_DIR/$EXE.objdump || echo 'riscv32-elf-objdump not found, skipping OBJDUMP'
-socat $USB,rawer,b115200 STDOUT | defmt-print --verbose -e $OUT_DIR/$EXE --log-format "$LOG_FORMAT"
+socat $USB,rawer,b115200 STDOUT | defmt-print --verbose -e $OUT_DIR/$EXE --log-format "$LOG_FORMAT" | tee usb.log
