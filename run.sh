19345d691c495fd6f6e01bfc245b2e502404ab4e
Codetector <codetector@codetector.org>
2024-10-04 21:26:00 -0700

    update main to a "real device"

--- run.sh
+++ run.sh
@@ -10,17 +10,21 @@ LOG_FILE="usb.log"
 
 OBJDUMP=riscv32-elf-objdump
 
-if [ ! -x $OBJDUMP ]; then
+if [ ! -x `which $OBJDUMP` ]; then
+echo 'using rv64 objdump'
 OBJDUMP=riscv64-elf-objdump
 fi
 
 EXE=vapor_keeb
 OUT_DIR=target/riscv32imac-unknown-none-elf/release
 
+if [ "${CAT:-}" == "" ] ; then
 cargo build --release
-$OBJDUMP -dC $OUT_DIR/$EXE > $OUT_DIR/$EXE.objdump || echo 'riscv32-elf-objdump not found, skipping OBJDUMP'
+$OBJDUMP -dC $OUT_DIR/$EXE > $OUT_DIR/$EXE.objdump || echo "$OBJDUMP not found, skipping OBJDUMP"
 probe-rs download --chip CH32V307 $OUT_DIR/$EXE
 probe-rs reset --chip CH32V307
+fi
+
 if [[ "$OS" = "Darwin" ]] ; then
 (stty speed 115200 >/dev/null && cat) <$UART | defmt-print --verbose -e $OUT_DIR/$EXE --log-format "$LOG_FORMAT" | tee $LOG_FILE
 else
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 use core::{mem::MaybeUninit, panic::PanicInfo};
 
-use ch32_hal::otg_fs::{Driver, EndpointDataBuffer};
+use ch32_hal::otg_fs::{self, Driver, EndpointDataBuffer};
 use ch32_hal::{self as hal};
 use ch32_hal::{
     mode::Blocking,
@@ -15,10 +15,18 @@ use ch32_hal::{
 };
 use defmt::{info, println, Display2Format};
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_time::{Duration, Instant, Timer};
-use embassy_usb::Builder;
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::control::{InResponse, OutResponse, Recipient, Request, RequestType};
+use embassy_usb::msos::{self, windows_version};
+use embassy_usb::types::InterfaceNumber;
+use embassy_usb::{Builder, Handler};
+use embassy_usb_driver::EndpointError;
 use hal::gpio::{AnyPin, Level, Output, Pin};
 use logger::set_logger;
+
+const DEVICE_INTERFACE_GUIDS: &[&str] = &["{DAC2087C-63FA-458D-A55D-827C0762DEC7}"];
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -73,24 +81,135 @@ async fn main(spawner: Spawner) -> ! {
     /* USB DRIVER SECION */
     let mut buffer: [EndpointDataBuffer; 4] = [EndpointDataBuffer::default(); 4];
     let driver = Driver::new(p.OTG_FS, p.PA12, p.PA11, &mut buffer);
-    let config = embassy_usb::Config::new(0xBADF, 0xbeef);
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-raw");
+    config.serial_number = Some("12345678");
+
+    // Required for windows compatibility.
+    // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
     let mut config_descriptor = [0; 256];
     let mut bos_descriptor = [0; 256];
-    let mut control_buf = [0; 7];
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut handler = ControlHandler {
+        if_num: InterfaceNumber(0),
+    };
+
     let mut builder = Builder::new(
         driver,
         config,
         &mut config_descriptor,
         &mut bos_descriptor,
-        &mut [], // no msos descriptors
+        &mut msos_descriptor,
         &mut control_buf,
     );
-    let mut usb_device = builder.build();
+
+
+
+    builder.msos_descriptor(windows_version::WIN8_1, 0);
+    builder.msos_feature(msos::CompatibleIdFeatureDescriptor::new("WINUSB", ""));
+    builder.msos_feature(msos::RegistryPropertyFeatureDescriptor::new(
+        "DeviceInterfaceGUIDs",
+        msos::PropertyData::RegMultiSz(DEVICE_INTERFACE_GUIDS),
+    ));
+
+
+    // Add a vendor-specific function (class 0xFF), and corresponding interface,
+    // that uses our custom handler.
+    let mut function = builder.function(0xFF, 0, 0);
+    let mut interface = function.interface();
+    let _alternate = interface.alt_setting(0xFF, 0, 0, None);
+    handler.if_num = interface.interface_number();
+    drop(function);
+    builder.handler(&mut handler);
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    // usb.run().await;
+
+    // let mut builder = Builder::new(
+    //     driver,
+    //     config,
+    //     &mut config_descriptor,
+    //     &mut bos_descriptor,
+    //     &mut [], // no msos descriptors
+    //     &mut control_buf,
+    // );
+    // let mut usb_device = builder.build();
     loop {
-        usb_device.run_until_suspend().await;
+        usb.run_until_suspend().await;
         println!("USB Suspended at: {}ms", Instant::now().as_millis());
-        Timer::after_millis(100).await;
+        usb.wait_resume().await;
+        println!("USB Resumed at: {}ms", Instant::now().as_millis());
     }
     /* END USB DRIVER */
+    loop {
+        panic!("how are we here");
+    }
+}
 
+/// Handle CONTROL endpoint requests and responses. For many simple requests and responses
+/// you can get away with only using the control endpoint.
+struct ControlHandler {
+    if_num: InterfaceNumber,
+}
+
+impl Handler for ControlHandler {
+    /// Respond to HostToDevice control messages, where the host sends us a command and
+    /// optionally some data, and we can only acknowledge or reject it.
+    fn control_out<'a>(&'a mut self, req: Request, buf: &'a [u8]) -> Option<OutResponse> {
+        // Log the request before filtering to help with debugging.
+        info!("Got control_out, request={}, buf={:a}", req, buf);
+
+        // Only handle Vendor request types to an Interface.
+        if req.request_type != RequestType::Vendor || req.recipient != Recipient::Interface {
+            return None;
+        }
+
+        // Ignore requests to other interfaces.
+        if req.index != self.if_num.0 as u16 {
+            return None;
+        }
+
+        // Accept request 100, value 200, reject others.
+        if req.request == 100 && req.value == 200 {
+            Some(OutResponse::Accepted)
+        } else {
+            Some(OutResponse::Rejected)
+        }
+    }
+
+    /// Respond to DeviceToHost control messages, where the host requests some data from us.
+    fn control_in<'a>(&'a mut self, req: Request, buf: &'a mut [u8]) -> Option<InResponse<'a>> {
+        info!("Got control_in, request={}", req);
+
+        // Only handle Vendor request types to an Interface.
+        if req.request_type != RequestType::Vendor || req.recipient != Recipient::Interface {
+            return None;
+        }
+
+        // Ignore requests to other interfaces.
+        if req.index != self.if_num.0 as u16 {
+            return None;
+        }
+
+        // Respond "hello" to request 101, value 201, when asked for 5 bytes, otherwise reject.
+        if req.request == 101 && req.value == 201 && req.length == 5 {
+            buf[..5].copy_from_slice(b"hello");
+            Some(InResponse::Accepted(&buf[..5]))
+        } else {
+            Some(InResponse::Rejected)
+        }
+    }
 }
