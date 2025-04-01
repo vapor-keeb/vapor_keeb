e791f70c8e9d0d93ef0f0082d9e3dbae402d0ae0
codetector <codetector@codetector.org>
2025-03-31 21:49:26 -0700

    move things from hosttest to async-usb-host

--- src/bin/hosttest.rs
+++ src/bin/hosttest.rs
@@ -8,8 +8,9 @@ use core::{mem::MaybeUninit, panic::PanicInfo};
 
 use async_usb_host::driver::kbd::HidKbd;
 use async_usb_host::errors::UsbHostError;
+use async_usb_host::futures::select_pin2;
 use async_usb_host::pipe::USBHostPipe;
-use async_usb_host::Driver;
+use async_usb_host::HostDriver;
 use async_usb_host::Host;
 use ch32_hal::i2c::I2c;
 use ch32_hal::otg_fs::{self};
@@ -25,8 +26,8 @@ use ch32_hal::{
 };
 use defmt::error;
 use defmt::{info, println};
-use driver::USBHostDriver;
 use embassy_executor::Spawner;
+use embassy_futures::select::Either;
 use embassy_time::Timer;
 use vapor_keeb::logger::set_logger;
 
@@ -52,130 +53,6 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 static mut LOGGER_UART: MaybeUninit<UartTx<'static, USART1, Blocking>> = MaybeUninit::uninit();
-
-/// Future for the [`select_array`] function.
-#[derive(Debug)]
-#[must_use = "futures do nothing unless you `.await` or poll them"]
-pub struct SelectPinArray<'a, 'b, Fut, const N: usize> {
-    inner: &'a mut Pin<&'b mut [Option<Fut>; N]>,
-}
-
-/// Creates a new future which will select over an array of futures.
-///
-/// The returned future will wait for any future to be ready. Upon
-/// completion the item resolved will be returned, along with the index of the
-/// future that was ready.
-///
-/// If the array is empty, the resulting future will be Pending forever.
-pub fn select_pin_array<'a, 'b, Fut: Future, const N: usize>(
-    arr: &'a mut Pin<&'b mut [Option<Fut>; N]>,
-) -> SelectPinArray<'a, 'b, Fut, N> {
-    SelectPinArray { inner: arr }
-}
-
-impl<'a, 'b, Fut: Future, const N: usize> Future for SelectPinArray<'a, 'b, Fut, N> {
-    type Output = (Fut::Output, usize);
-
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // Safety: Since `self` is pinned, `inner` cannot move. Since `inner` cannot move,
-        // its elements also cannot move. Therefore it is safe to access `inner` and pin
-        // references to the contained futures.
-        let item = unsafe { self.get_unchecked_mut().inner.as_mut().get_unchecked_mut() }
-            .iter_mut()
-            .enumerate()
-            .find_map(|(i, f)| {
-                f.as_mut()
-                    .and_then(|f| match unsafe { Pin::new_unchecked(f) }.poll(cx) {
-                        Poll::Pending => None,
-                        Poll::Ready(e) => Some((i, e)),
-                    })
-                    .inspect(|_| *f = None)
-            });
-
-        match item {
-            Some((idx, res)) => Poll::Ready((res, idx)),
-            None => Poll::Pending,
-        }
-    }
-}
-
-mod driver {
-    use core::{future::Future, pin::Pin};
-
-    use super::select_pin_array;
-    use arrayvec::ArrayVec;
-    use async_usb_host::{
-        descriptor::DeviceDescriptor, driver::kbd::HidKbd, errors::UsbHostError, pipe::USBHostPipe,
-        DeviceHandle, Driver,
-    };
-    use defmt::{error, panic, trace, unwrap};
-    use embassy_futures::select::{select, Either};
-    use embassy_sync::{
-        blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel, mutex::Mutex,
-    };
-
-    pub type DeviceChannel = Channel<CriticalSectionRawMutex, (DeviceHandle, DeviceDescriptor), 1>;
-
-    pub struct USBHostDriver<'a, D: Driver, const MAX_DEVICES: usize = 4> {
-        pipe: &'a USBHostPipe<D, 16>,
-        new_dev: &'a DeviceChannel,
-    }
-
-    impl<'a, D: Driver, const MAX_DEVICES: usize> USBHostDriver<'a, D, MAX_DEVICES> {
-        pub fn new(pipe: &'a USBHostPipe<D, 16>, new_dev: &'a DeviceChannel) -> Self {
-            Self { pipe, new_dev }
-        }
-
-        pub async fn run<
-            Fut: Future<Output = Result<(), UsbHostError>>,
-            F: Fn(HidKbd, &'a USBHostPipe<D, 16>) -> Fut,
-        >(
-            &mut self,
-            f: F,
-        ) {
-            let mut device_futures: [Option<Fut>; MAX_DEVICES] = core::array::from_fn(|_| None);
-            let mut pinned: Pin<&mut [Option<Fut>; MAX_DEVICES]> =
-                unsafe { Pin::new_unchecked(&mut device_futures) };
-
-            loop {
-                let new_dev_fut = self.new_dev.receive();
-                match select(new_dev_fut, select_pin_array(&mut pinned)).await {
-                    Either::First((device, descriptor)) => {
-                        let kbd = HidKbd::try_attach(self.pipe, device, descriptor).await;
-                        match kbd {
-                            Ok(kbd) => {
-                                // Find an empty slot for the new device
-                                if let Some(empty_slot) =
-                                    unsafe { pinned.as_mut().get_unchecked_mut() }
-                                        .iter_mut()
-                                        .position(|slot| slot.is_none())
-                                {
-                                    unsafe {
-                                        pinned.as_mut().get_unchecked_mut()[empty_slot] =
-                                            Some(f(kbd, self.pipe));
-                                    }
-                                    trace!("Device added to slot {}", empty_slot);
-                                } else {
-                                    error!("No empty slots available for new device");
-                                }
-                            }
-                            Err(e) => {
-                                error!("Failed to attach keyboard: {}", e);
-                            }
-                        }
-                    }
-                    Either::Second((result, idx)) => {
-                        match result {
-                            Ok(_) => trace!("Device at slot {} completed successfully", idx),
-                            Err(e) => error!("Device error at slot {}: {}", idx, e),
-                        }
-                        // The slot is already cleared by the select_pin_array implementation
-                    }
-                }
-            }
-        }
-    }
-}
 
 #[embassy_executor::main(entry = "qingke_rt::entry")]
 async fn main(_spawner: Spawner) -> ! {
@@ -257,21 +134,21 @@ async fn main(_spawner: Spawner) -> ! {
     let pipe: USBHostPipe<USBHsHostDriver<'_, _>, 16> = USBHostPipe::new(pipe);
 
     // Create the device channel
-    let new_dev_channel = driver::DeviceChannel::new();
+    let new_dev_channel = async_usb_host::driver::DeviceChannel::new();
 
     // Create our host driver with support for multiple devices
-    let mut host_driver = USBHostDriver::<_, 8>::new(&pipe, &new_dev_channel);
+    let mut host_driver = async_usb_host::driver::USBHostDriver::<_, 8>::new(&pipe, &new_dev_channel);
     let mut host = Host::<'_, _, 4, 16>::new(bus, &pipe);
 
     // Create a keyboard handler function
-    async fn kbd_handler<'a, D: Driver>(
+    async fn kbd_handler<'a, D: HostDriver>(
         kbd: HidKbd,
         pipe: &'a USBHostPipe<D, 16>,
     ) -> Result<(), UsbHostError> {
         info!("Keyboard connected!");
         // Handle keyboard events here
         kbd.run(pipe).await
-    };
+    }
 
     // Create the futures for host_driver and host
     let mut host_driver_fut = Some(host_driver.run(kbd_handler));
@@ -280,9 +157,6 @@ async fn main(_spawner: Spawner) -> ! {
     // Pin the futures
     let mut pinned_host_driver_fut = unsafe { Pin::new_unchecked(&mut host_driver_fut) };
     let mut pinned_host_fut = unsafe { Pin::new_unchecked(&mut host_fut) };
-
-    // Import the select_pin2 function
-    use vapor_keeb::selectpin2::{select_pin2, Either};
 
     loop {
         match select_pin2(&mut pinned_host_driver_fut, &mut pinned_host_fut).await {
