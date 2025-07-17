2b68ebb0680e518c903f47436bcc65cea1b719bc
Codetector <codetector@codetector.org>
2025-07-16 23:40:32 -0700

    update shell.nix to use the right mkShell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,12 @@
 { pkgs ? import <nixpkgs> {}}:
-   pkgs.pkgsCross.riscv32-embedded.mkShell {
-     buildInputs = [];
+   pkgs.mkShell {
      nativeBuildInputs = [
+      pkgs.lld
       pkgs.pkgsCross.riscv32-embedded.buildPackages.gdb
-    ];
+      pkgs.pkgsCross.riscv32-embedded.buildPackages.gcc
+      ];
+      shellHooks = ''
+        alias riscv32-unknown-elf-gcc='riscv32-none-elf-gcc'
+        alias riscv32-unknown-elf-g++='riscv32-none-elf-g++'
+      '';
    }
